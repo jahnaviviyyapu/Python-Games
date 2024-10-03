362b878776fcb6eae0fa0fea48a6cbecfe6425a3
Jahnavi Viyyapu <jahnaviviyyapu@gmail.com>
2024-10-03 13:22:56 +0530

    Added Ping Pong game

---
+++ Ping Pong Game/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))  # positions are the arguments sent to the init function which is used in goto method.
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+is_game_on = True
+while is_game_on:
+    time.sleep(ball.move_speed) # to pause the while loop for 0.1 for each iteration
+    screen.update()
+    ball.move()
+
+    # Detect collision with top and bottom wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when paddles misses the ball
+    if ball.xcor() > 380:  # right paddle
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:  # left paddle
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
